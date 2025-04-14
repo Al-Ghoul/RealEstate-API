3ada181e92922ba1c517fd4cfaa313296d77b24b
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2025-04-14 07:31:49 +0200

    feat: add initial project files

---
+++ app/src/server.ts
@@ -0,0 +1,9 @@
+import { app } from "./app";
+import { env } from "./env";
+
+console.log(`Server running on port ${env.PORT.toString()}`);
+console.log(`Environment: ${env.NODE_ENV}`);
+
+app.listen(env.PORT, () => {
+  console.log(`Server is running on http://localhost:${env.PORT.toString()}`);
+});
