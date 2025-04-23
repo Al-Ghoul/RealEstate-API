e24a0bdeec1569d87c5fdd2d3cce595498d7a43b
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2025-04-23 15:33:53 +0200

    feat: add linter

---
+++ app/eslint.config.mjs
@@ -0,0 +1,39 @@
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    ignores: [
+      "dist/",
+      "build/",
+      "node_modules/",
+      "drizzle.config.ts",
+      "eslint.config.mjs",
+      "src/tests/",
+      "jest.config.ts",
+      "babel.config.js",
+      "src/db/dropDatabase.ts",
+    ],
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+];
