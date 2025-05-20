b19383c4e7704935f0a6b38f8257711c686f2953
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2025-05-20 09:15:13 +0300

    chore: add logging for input schema middleware

--- app/src/middlewares/errorHandler.middleware.ts
+++ app/src/middlewares/errorHandler.middleware.ts
@@ -2,6 +2,7 @@ import { type Request, type Response, type NextFunction } from "express";
 import multer from "multer";
 import type { Locales } from "../i18n/i18n-types";
 import L from "../i18n/i18n-node";
+import { logger } from "../utils/logger.utils";
 
 export function errorHandlerMiddleware(
   err: Error | multer.MulterError | null,
@@ -19,6 +20,16 @@ export function errorHandlerMiddleware(
         details: L[lang].UNABLE_TO_UPLOAD_IMAGE_DETAILS(),
       });
     } else if (err instanceof Error) {
+      logger.error({
+        route: req.originalUrl,
+        message: "Internal server error",
+        info: {
+          requestId: req.id,
+          error: err.message,
+          stack: err.stack,
+          browser: req.headers["user-agent"],
+        },
+      });
       res.status(400).json({
         requestId: req.id,
         message: L[lang].UNABLE_TO_UPLOAD_IMAGE(),
